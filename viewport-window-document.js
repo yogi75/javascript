691b1afc5bd84a2691f064c96ddf8e46cd6de5ed
Yogendra Pandey <65703176+yogi75@users.noreply.github.com>
2020-07-16 00:09:28 +0530

    viewport vs window vs document

---
+++ viewport-window-document.js
@@ -0,0 +1,13 @@
+Viewport is the rectangle area where things are visible to you. The document can be 
+larger than that and you'll see scrollbars if so.
+
+Viewport: It is your device screen.
+
+Window: It is your browser window. The window can be as big as viewport or smaller.
+
+Document: It is the webpage. It can be bigger than viewport or even bigger than window.
+
+Notes: Some websites are for not created for mobiles. Hence the webpage/document is much 
+bigger than the mobile viewport and you have to swipe to see the parts that spill outside 
+the screen. On a desktop, you can make the window of your browser smaller or same as the 
+viewport/monitor.
