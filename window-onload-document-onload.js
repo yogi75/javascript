5b67afd9c41e976a8e47afb7d57f3288424026d0
Yogendra Pandey <65703176+yogi75@users.noreply.github.com>
2020-07-16 00:19:48 +0530

    window vs document onload event

---
+++ window-onload-document-onload.js
@@ -0,0 +1,9 @@
+window.onload
+
+By default, it is fired when the entire page loads, including its content (images, CSS, scripts, etc.).
+
+In some browsers it now takes over the role of document.onload and fires when the DOM is ready as well.
+
+document.onload
+
+It is called when the DOM is ready which can be prior to images and other external content is loaded.
