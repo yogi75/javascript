935e2b34fa1615760f5af5e5b310f02f2bf01742
Yogendra Pandey <65703176+yogi75@users.noreply.github.com>
2024-07-10 19:46:15 +0530

    Update and rename reduce-map-filter-array-pollyfill.js to reduce-map-filter-sort-array-pollyfill.js

--- reduce-map-filter-sort-array-pollyfill.js
+++ reduce-map-filter-sort-array-pollyfill.js
@@ -40,3 +40,14 @@ Array.prototype.filterArray = function(callback){
     return temp;
 }
 console.log([1,2,3,4].filterArray(num => num > 3)) // [4]
+
+// Sort polyfill
+
+Array.prototype.doSort = function(){
+  return this.sort(function(a,b){
+    return a-b;
+  })
+}
+
+var a = [1,2,10,5,11,4].doSort();
+console.log(a); // [1, 2, 4, 5, 10, 11]
