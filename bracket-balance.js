7ccf49b4d30467febbf40a09fd80d46d19e2b538
Yogendra Pandey <65703176+yogi75@users.noreply.github.com>
2024-07-24 01:01:02 +0530

    Update bracket-balance.js

--- bracket-balance.js
+++ bracket-balance.js
@@ -9,3 +9,37 @@ const balancedBracket = (str)=>{
 }
 
 console.log(balancedBracket('({{}})[] { } ([]{})')) // true
+
+/**  if balanced bracket return 1 and total pair
+*    if no bracket in string return 1
+*    if no balanced bracket return 0
+*/
+function SearchingChallenge(str) { 
+
+  // code goes here  
+  var result={count:0, pair:0, noBracket:0} 
+  var obj = str.split("").reduce((result, char)=>{
+    if (char == "(" || char == "{" || char == "["){
+      result.count++;
+      result.noBracket = 1;
+      if(result.count == 0){
+        result.pair++;
+      }
+    } else if(char == ")" || char == "}" || char == "]"){
+      result.count--;
+      result.noBracket = 1;
+      if(result.count == 0){
+        result.pair++;
+      }
+    };
+  return result;
+  }, result); 
+  if(!obj.noBracket) return 1;
+  if(!obj.count) return `1 ${obj.pair}`;
+  return 0;
+}
+   
+// keep this function call here 
+console.log(SearchingChallenge("(dfgf)[!]")); // 1 2
+
+
